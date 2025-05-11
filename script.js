fe354b6cdf6c01b71bc205023c2e3c067aa602f5
gisha <52324253+gisha313@users.noreply.github.com>
2025-05-11 18:05:20 +0200

    Add funcs to create book cards and display them

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const myLibrary = [];
 
-function Book(id, title, author, pages, isRead) {
+function Book(title, author, pages, isRead) {
   if (!new.target) throw new Error("Constructor called without new operator.");
 
   this.id = crypto.randomUUID();
@@ -13,4 +13,37 @@ function Book(id, title, author, pages, isRead) {
 function addBookToLibrary(title, author, pages, isRead) {
   let newBook = new Book(title, author, pages, isRead);
   myLibrary.push(newBook);
+}
+
+function createBookCard(book) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const title = document.createElement("h2");
+  title.classList.add("book-title");
+  title.textContent = book.title;
+  card.appendChild(title);
+
+  const author = document.createElement("p");
+  author.textContent = "by " + book.author;
+  card.appendChild(author);
+
+  const pages = document.createElement("p");
+  pages.textContent = book.pages + " pages";
+  card.appendChild(pages);
+
+  const isRead = document.createElement("p");
+  isRead.textContent = book.isRead ? "Read" : "Not Read";
+  card.appendChild(isRead);
+
+  return card;
+}
+
+const cardContainer = document.querySelector(".card-container");
+
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    const bookCard = createBookCard(book);
+    cardContainer.appendChild(bookCard);
+  });
 }
