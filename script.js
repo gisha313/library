1b3dd21a3f8c202929901c1aa78472c43f597e52
gisha <52324253+gisha313@users.noreply.github.com>
2025-05-12 16:18:59 +0200

    Add delete book btn to creation of book cards

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-const myLibrary = [];
+const myLibrary = [
+  new Book("A Clockwork Orange", "Anthony Burgees", 240, false),
+  new Book("Brave New World", "Aldous Huxley", 200, true),
+  new Book("The Stranger", "Albert Camus", 100, true),
+  new Book("Catcher in the Rye", "J. D. Salinger", 210, true),
+];
+
+const cardContainer = document.querySelector(".card-container");
+const formContainer = document.querySelector(".form-container");
+const callBookFormBtn = document.querySelector("#call-book-form");
+const addBookBtn = document.querySelector("#add-book");
+
+const formBook = document.querySelector(".book-form > form");
+const formTitle = document.querySelector("#title");
+const formAuthor = document.querySelector("#author");
+const formPages = document.querySelector("#pages");
+const formRead = document.querySelector("#isRead");
+
+displayBooks();
 
 function Book(title, author, pages, isRead) {
   if (!new.target) throw new Error("Constructor called without new operator.");
@@ -24,6 +42,11 @@ function createBookCard(book) {
   title.textContent = book.title;
   card.appendChild(title);
 
+  const deleteBookBtn = document.createElement("button");
+  deleteBookBtn.classList.add("delete-book");
+  deleteBookBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>close-thick</title><path d="M20 6.91L17.09 4L12 9.09L6.91 4L4 6.91L9.09 12L4 17.09L6.91 20L12 14.91L17.09 20L20 17.09L14.91 12L20 6.91Z" /></svg>`;
+  card.appendChild(deleteBookBtn);
+
   const author = document.createElement("p");
   author.textContent = "by " + book.author;
   card.appendChild(author);
@@ -38,17 +61,6 @@ function createBookCard(book) {
 
   return card;
 }
-
-const cardContainer = document.querySelector(".card-container");
-const formContainer = document.querySelector(".form-container");
-const callBookFormBtn = document.querySelector("#call-book-form");
-const addBookBtn = document.querySelector("#add-book");
-
-const formBook = document.querySelector(".book-form > form");
-const formTitle = document.querySelector("#title");
-const formAuthor = document.querySelector("#author");
-const formPages = document.querySelector("#pages");
-const formRead = document.querySelector("#isRead");
 
 function displayBooks() {
   cardContainer.innerHTML = "";
